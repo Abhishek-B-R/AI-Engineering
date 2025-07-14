8216ef3ea7ed50f3a8fad95770a94628f8faef17
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-15 00:12:01 +0530

    added vector db via supabase and stored some sample data in form of vector embeddings

---
+++ 7.Vector-DB-via-Supabase/content.js
@@ -0,0 +1,13 @@
+// Fake podcast titles and descriptions
+export default [
+  "Beyond Mars (1 hr 15 min)",
+  "Jazz under stars (55 min)",
+  "Mysteries of the deep (1 hr 30 min)",
+  "Rediscovering lost melodies (48 min)",
+  "Tales from the tech frontier (1 hr 5 min)",
+  "The soundscape of silence (30 min)",
+  "Decoding dreams (1 hr 22 min)",
+  "Time capsules (50 min)",
+  "Frozen in time (1 hr 40 min)",
+  "Songs of the Sea (1 hr)"
+];
