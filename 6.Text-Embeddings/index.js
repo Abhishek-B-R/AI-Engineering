723461da918e3ffd5f4b681d054d7e1880c50b98
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-14 23:08:39 +0530

    assignment to pair text with embeddings solved

--- 6.Text-Embeddings/index.js
+++ 6.Text-Embeddings/index.js
@@ -1,12 +1,47 @@
 import openai from './config.js';
 
-/** Create embeddings representing the input text */
+// trial 1
+// async function main() {
+//   const embedding = await openai.embeddings.create({
+//     model: "text-embedding-ada-002",
+//     input: "Hello, world!",
+//   });
+//   console.log(embedding);
+//   console.log(embedding.data[0].embedding);
+// }
+// main();
+
+
+// trial 2
+/*
+  Challenge: Pair text with its embedding
+    - For each text input, create an object with 
+      a 'content' and 'embedding' property
+    - The value of 'content' should be the text
+    - The value of 'embedding' should be the vector embedding for that text
+*/
+
+const content = [
+  "Beyond Mars: speculating life on distant planets.",
+  "Jazz under stars: a night in New Orleans' music scene.",
+  "Mysteries of the deep: exploring uncharted ocean caves.",
+  "Rediscovering lost melodies: the rebirth of vinyl culture.",
+  "Tales from the tech frontier: decoding AI ethics.",
+]; 
+
+let textEmbeddingPairs = []
 async function main() {
   const embedding = await openai.embeddings.create({
     model: "text-embedding-ada-002",
-    input: "Hello, world!",
+    input: content,
   });
-  console.log(embedding);
-  console.log(embedding.data[0].embedding);
+  embedding.data.forEach((item,index)=>{
+    textEmbeddingPairs.push({
+      content: content[index],
+      embedding: item.embedding
+    });                                                         
+  })
+  console.log(textEmbeddingPairs);
 }
 main();
+// charged me 0.000006 USD 
