1c45f4ea887f05dc390437eeefde0316e59e2c6a
Abhishek-B-R <byteblaster1468@gmail.com>
2025-07-15 23:27:35 +0530

    added semantic search

--- 7.Vector-DB-via-Supabase/index.js
+++ 7.Vector-DB-via-Supabase/index.js
@@ -1,22 +1,41 @@
 import { openai, supabase } from './config.js';
 import podcasts from './content.js';
 
-async function main(input) {
-  const data = await Promise.all(
-    input.map( async (textChunk) => {
-        const embeddingResponse = await openai.embeddings.create({
-            model: "text-embedding-ada-002",
-            input: textChunk
-        });
-        return { 
-          content: textChunk, 
-          embedding: embeddingResponse.data[0].embedding 
-        }
+// async function main(input) {
+//   const data = await Promise.all(
+//     input.map( async (textChunk) => {
+//         const embeddingResponse = await openai.embeddings.create({
+//             model: "text-embedding-ada-002",
+//             input: textChunk
+//         });
+//         return { 
+//           content: textChunk, 
+//           embedding: embeddingResponse.data[0].embedding 
+//         }
         
-    })    
-);
-    // insert content and embedding into Supabase
-    await supabase.from('documents').insert(data)
-    console.log('Embedding and storing complete!');
+//     })    
+// );
+//     // insert content and embedding into Supabase
+//     await supabase.from('documents').insert(data)
+//     console.log('Embedding and storing complete!');
+// }
+// main(podcasts)
+
+// doing semantic search
+const query = "something peaceful and relaxing"
+async function search(query){
+  const embeddingResponse = await openai.embeddings.create({
+    model: "text-embedding-ada-002",
+    input: query
+  })
+
+  const embedding = embeddingResponse.data[0].embedding;
+
+  const { data } = await supabase.rpc('match_documents', {
+    query_embedding: embedding,
+    match_threshold : 0.5,
+    match_count: 5
+  })
+  console.log(data)
 }
-main(podcasts)
+search(query)
